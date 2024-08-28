1ff8726041b6eb7dfa5584db11305a4f2e4693e0
tgraber4 <tgraber4@gmail.com>
2024-08-28 15:11:56 -0500

    Finished Account main page

--- components/accounts/accounts.js
+++ components/accounts/accounts.js
@@ -28,6 +28,12 @@ var createAccount = function (name, balance, type, backgroundOption) {
 
     const button1 = document.createElement("button");
 
+    if (type === "credit") {
+
+      button1.onclick = function () { openModal();};
+
+    }
+
     const p4 = document.createElement("p");
 
     const node3 = document.createTextNode(types[type]["under_info"]);
@@ -95,6 +101,125 @@ createAccount("Credit Card", 80.78, "credit", 1);
 createAccount("Rewards", 80.78, "rewards", 3);
 createBlankAccount();
 
+
+
+var modal = document.getElementById("myModal");
+
+var close = document.getElementsByClassName("close")[0];
+
+close.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    closeModal();
+  }
+  if (event.target != document.getElementsByClassName("custom-amount")[0]) {
+    const element = document.getElementsByClassName("custom-amount")[0];
+    element.placeholder = "0.00";
+    if (mode === "modal") {
+      updateCustomAmount();
+    }
+    if (element.value.length === 0 && payOptions["status"] === "custom") {
+      updateContinue("remove");
+      payOptions["status"] = "";
+      payOptions["payBalance"] = 0;
+    }
+  }
+}
+var mode = "none";
+var updateCustomAmount = function () {
+  var element = document.getElementsByClassName("custom-amount")[0]
+  if (element.value.length > 0 && element.value.includes(".") === false) {
+    element.value = element.value + ".00";
+  }
+  if (element.value.length > 0) {
+    var customValue = Number(element.value);
+    if (customValue > 0) {
+      payOptions["payBalance"] = Math.floor(customValue * 100) / 100; 
+      updateContinue("add");
+      payOptions["status"] = "custom";
+      resetBox(0);
+      resetBox(1);
+      element.blur();
+    }
+  }
+}
+document.addEventListener('keyup', event => {
+  if (event.code == "Enter" && mode === "modal") {
+    updateCustomAmount();
+  }
+})
+
+var openModal = function () {
+  modal.style.display = "block";
+  mode = "modal";
+  resetBox(0);
+  resetBox(1);
+  updateContinue("remove");
+  var statement = document.getElementById("statement");
+  var current = document.getElementById("current");
+  var custom_amount = document.getElementsByClassName("custom-amount")[0];
+  custom_amount.value = "";
+  statement.innerText = "$" + account["statement-balance"];
+  current.innerText = "$" + account["current-balance"];
+}
+var closeModal = function () {
+  modal.style.display = "none";
+  mode = "none";
+}
+
+var customAmount = function () {
+  const element = document.getElementsByClassName("custom-amount")[0];
+  element.placeholder = "";
+}
+
+
+var account = {"type":"Credit Card", "current-balance": 150.00, "statement-balance": 100.00};
+var payOptions = {"status": "", "payBalance": -1};
+var resetBox = function (num) {
+  var otherbox = document.getElementsByClassName("moneybox")[num];
+  otherbox.classList.remove("selected");
+}
+var updateContinue = function (conString) {
+  var continueBox = document.getElementsByClassName("continue")[0];
+  if (conString === "add") {
+    continueBox.classList.add("green_continue");
+  } else {
+    continueBox.classList.remove("green_continue");
+  }
+}
+var moneybox = function (num) {
+  if (payOptions["status"] != "custom") {
+    var box = document.getElementsByClassName("moneybox")[num];
+    box.classList.add("selected");
+    updateContinue("add");
+    if (num === 0) {
+      payOptions["payBalance"] = account["statement-balance"];
+      resetBox(1)
+      payOptions["status"] = "statement";
+    } else {
+      payOptions["payBalance"] = account["current-balance"];
+      resetBox(0)
+      payOptions["status"] = "current";
+    }
+  }
+}
+var payCredit = function () {
+  if (payOptions["payBalance"] > 0 && payOptions["payBalance"] <= account["current-balance"]) {
+    closeModal();
+      if (payOptions["payBalance"] >= account["statement-balance"]) {
+        account["statement-balance"] = 0;
+      } else {
+        account["statement-balance"] -= payOptions["payBalance"];
+      }
+      account["current-balance"] -= payOptions["payBalance"];
+    } else {
+      // send error message
+    }
+}
 
 
 /*
